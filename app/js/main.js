8fb25832a21eb3b0be6c69fd9e404d0dac31f7ae
Димон <D:\  >
2015-12-11 16:46:43 +0600

    popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,28 @@
 
 	console.log($('.main-footer'))
+	$('.b-close').click(hideModal);
+	$('.popup-bg').click(hideModal)
+	$('.add-project').click(showModal);
+	function showModal() {
+		$('.popup-container').show();
+		$('.popup-bg').show();
+
+	};
+	
+
+	function hideModal() {
+		$('.popup-container').css("display", "none");
+		$('.popup-bg').css("display", "none");
+		$(document).keypress(function(eventObject) {
+ 			console.log(eventObject);	
+	 	});
+
+};
+
+
+
+$('.button-submit').click(showSuccess);
+
+function showSuccess() {
+	$('.uploading-success').css('display','block');
+}
